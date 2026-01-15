abebe2e7e910781c5cc990ee36c7d9ec34af5cff
mahmoud08033 <mahmoudelfaramwy6@gmail.com>
2026-01-15 20:48:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let x=document.querySelector('#menu');
+let z = document.querySelector('.mark');
+let y=document.querySelector('.wrong');
+
+
+
+z.addEventListener('click', function ()  {
+ 
+//   x.style.display ='block '  
+  x.style.transform ='rotateY(0deg) '
+  
+  
+
+    
+
+
+})
+
+y.addEventListener('click', function ()  {
+    x.style.transform ='rotateY(90deg) '
+    e.stopPropagation();
+
+ 
+})
